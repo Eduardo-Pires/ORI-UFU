69dd505e62036dd3e313a97c9877cf425b649738
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-10-04 02:16:54 -0300

    geração de índice invertido

--- trabalho 1/modelo_booleano.py
+++ trabalho 1/modelo_booleano.py
@@ -14,6 +14,8 @@ pontuacao = list(string.punctuation) + ['...', "''", '\x97', '..', '....']
 extrator = nltk.stem.RSLPStemmer()
 
 with open(arquivoBase, 'r', encoding='utf-8') as bf:
+    indiceInvertido = {}
+    counter = 1
     base = bf.readlines()
     base = [linha.strip() for linha in base]
     for filePath in base:
@@ -21,7 +23,22 @@ with open(arquivoBase, 'r', encoding='utf-8') as bf:
             texto = arquivo.read()
             tokens = nltk.wordpunct_tokenize(texto)
             tokens = [extrator.stem(token) for token in tokens if token.lower() not in stopwords and token not in pontuacao]
-            print(tokens)
+            for token in tokens:
+                if token not in indiceInvertido:
+                    indiceInvertido[token] = [(counter, 1)]  # Usando uma lista de tuplas
+                else:
+                    # Verificando se o documento já está na lista
+                    doc_presente = False
+                    for i, (doc_id, count) in enumerate(indiceInvertido[token]):
+                        if doc_id == counter:
+                            doc_presente = True
+                            indiceInvertido[token][i] = (doc_id, count + 1)
+                            break
+                    if not doc_presente:
+                        indiceInvertido[token].append((counter, 1))
+        counter += 1
+    print(indiceInvertido)
+
 
 with open(arquivoConsulta, 'r', encoding='utf-8') as bc:
     consulta = bc.read()
