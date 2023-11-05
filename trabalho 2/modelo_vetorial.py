c38f0b6673e734fbb795240c1b2bfe7bc687f91b
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-11-04 21:25:22 -0300

    trabalho 2 finalizado

--- trabalho 2/modelo_vetorial.py
+++ trabalho 2/modelo_vetorial.py
@@ -1,14 +1,14 @@
 import nltk
 import string
 import sys
-import numpy
+import math
 
 stopwords = nltk.corpus.stopwords.words("portuguese")
 pontuacao = list(string.punctuation) + ['...', "''", '\x97', '..', '....', "!...", ",."]
 extrator = nltk.stem.RSLPStemmer()
 
 
-def gerador_de_tfidf(base_arquivo):
+def base_tfidf(base_arquivo):
     with open(base_arquivo, 'r', encoding='utf-8') as arquivos_de_base:
         base = arquivos_de_base.readlines()
         base = [linha.strip() for linha in base]
@@ -32,7 +32,7 @@ def gerador_de_tfidf(base_arquivo):
                 for token in set(tokens_base):
                     documentos_com_token[token] = documentos_com_token.get(token, 0) + 1
 
-                    tf = 1 + numpy.log10(tokens_base.count(token))
+                    tf = 1 + math.log10(tokens_base.count(token))
                     documentos_tfidf[documento][token] = tf
 
         for documento in base:
@@ -40,7 +40,7 @@ def gerador_de_tfidf(base_arquivo):
                 if token not in documentos_tfidf[documento]:
                     documentos_tfidf[documento][token] = 0
                 else:
-                    idf = numpy.log10(numero_de_documentos / documentos_com_token[token])
+                    idf = math.log10(numero_de_documentos / documentos_com_token[token])
                     documentos_tfidf[documento][token] *= idf
 
         with open("pesos.txt", "w", encoding='utf-8') as arquivo_de_pesos:
@@ -68,20 +68,22 @@ def consulta_tfidf(tokens, argumentos):
     termos_tfidf = {}
 
     for token in all_tokens:
-        tf = 1 + numpy.log10(tokens.count(token)) if token in tokens else 0
-        idf = numpy.log10(numero_de_documentos / documentos_com_token[token])
+        tf = 1 + math.log10(tokens.count(token)) if token in tokens else 0
+        idf = math.log10(numero_de_documentos / documentos_com_token[token])
 
         termos_tfidf[token] = tf * idf
 
     return termos_tfidf
 
 
-def similaridade(consulta_arquivo, argumentos):
+def similaridade_vetorial(base_arquivo, consulta_arquivo):
+    argumentos = base_tfidf(base_arquivo)
+
     base_ponderacao = argumentos["ponderaçãoTFIDF"]
     base = argumentos["baseDeDocumentos"]
     all_tokens = argumentos["listaDeTokens"]
 
-    with open(consulta_arquivo, 'r', encoding='utf-8') as arquivo_de_consulta:
+    with (open(consulta_arquivo, 'r', encoding='utf-8') as arquivo_de_consulta):
         consulta = arquivo_de_consulta.read()
 
         tokens_consulta = nltk.wordpunct_tokenize(consulta)
@@ -89,23 +91,33 @@ def similaridade(consulta_arquivo, argumentos):
                            if token not in stopwords and token != '&']
 
         consulta_ponderacao = consulta_tfidf(tokens_consulta, argumentos)
-        print(consulta_ponderacao)
+        documento_similaridade = {documento: 0 for documento in base}
+        documentos_validos = 0
+
+        for documento in base:
+            multiplica = 0
+            quadrado_base = 0
+            quadrado_consulta = 0
+            for token in set(all_tokens):
+                wij = base_ponderacao[documento][token]
+                wiq = consulta_ponderacao[token]
+
+                multiplica += wij * wiq
+                quadrado_base += wij ** 2
+                quadrado_consulta += wiq ** 2
+
+            documento_similaridade[documento] = multiplica / (math.sqrt(quadrado_consulta) * math.sqrt(quadrado_base))
+            documentos_validos += 1 if documento_similaridade[documento] >= 0.001 else 0
+
+        documentos_similaridade_ordenado = sorted(documento_similaridade.items(), key=lambda x: x[1], reverse=True)
+
+        with open("resposta.txt", "w", encoding='utf-8') as resposta_arquivo:
+            resposta_arquivo.write(f"{documentos_validos}\n")
+            for documento, similaridade in documentos_similaridade_ordenado:
+                if similaridade >= 0.001:
+                    resposta_arquivo.write(f"{documento}: {similaridade}\n")
 
 
-"""
-     resultados = []
-
-    resultadoFinal = [tupla[0] for tupla in resultados]
-
-    def intersection(resultado, adicao):
-        return list(set(resultado) & set(adicao))
-        
-   #     with open("resposta.txt", "w", encoding='utf-8') as respostaArquivo:
-            respostaArquivo.write(f"{len(resultadoFinal)}\n")
-            for idDocumento in resultadoFinal:
-                respostaArquivo.write(f"{base[idDocumento - 1]}\n")
-
-"""
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Entrada errada, favor chamar:  > python modelo_vetorial.py base.txt consulta.txt")
@@ -114,4 +126,4 @@ if __name__ == "__main__":
     arquivo_base = sys.argv[1]
     arquivo_consulta = sys.argv[2]
 
-    similaridade(arquivo_consulta, gerador_de_tfidf(arquivo_base))
+    similaridade_vetorial(arquivo_base, arquivo_consulta)
