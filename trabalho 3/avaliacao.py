9a882cd180dacd98e1332945539092b27410b68a
Eduardo Pires <94654833+Eduardo-Pires@users.noreply.github.com>
2023-11-30 22:32:40 -0300

    calculo precisão e revocação dos arquivos especificos

--- trabalho 3/avaliacao.py
+++ trabalho 3/avaliacao.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-
+from functools import reduce
 
 def pre_processamento(referencia_arquivo):
     with open(referencia_arquivo, 'r', encoding='utf-8') as ref:
@@ -10,53 +10,42 @@ def pre_processamento(referencia_arquivo):
         numero_de_referencias = int(base[0])
 
         respostas_ideais = [vetor.split() for vetor in base[1:numero_de_referencias + 1]]
-        respostas_ideais = [[int(valor) for valor in vetor] for vetor in respostas_ideais]
 
         respostas_do_sistema = [vetor.split() for vetor in base[numero_de_referencias + 1:]]
-        respostas_do_sistema = [[int(valor) for valor in vetor] for vetor in respostas_do_sistema]
 
         return {
+            "ref_num": numero_de_referencias,
             "referencia": respostas_ideais,
             "sistema": respostas_do_sistema
         }
-
-#def intersection(relevantes, algoritmo):
-#    return list(set(relevantes) & set(algoritmo))
-
 
 def cobertura(referencia_arquivo):
     respostas = pre_processamento(referencia_arquivo)
     respostas_ideais = respostas["referencia"]
     respostas_do_sistema = respostas["sistema"]
+    precisao = []
+    revocacao = []
 
+    for i in range(respostas["ref_num"]):
+        tamanho_ideais = len(respostas_ideais[i])
+        count = 0
+        aux_precisao = [0] * len(respostas_do_sistema[i])
+        aux_revocacao = [0] * len(respostas_do_sistema[i])
+        for j in range(len(respostas_do_sistema[i])):
+            if respostas_do_sistema[i][j] in respostas_ideais[i]:
+                count += 1
+                aux_precisao[j] = count / (j + 1)
+                aux_revocacao[j] = count / tamanho_ideais
 
-"""    
-   #with open("pesos.txt", "w", encoding='utf-8') as arquivo_de_pesos:
-            for documento, termos in documentos_tfidf.items():
-                sequencia_de_termos = ' '.join([f"{termo},{peso}" for termo, peso in termos.items() if peso != 0])
-                linha_pesos_de_documento = f"{documento}: {sequencia_de_termos}\n"
-
-                arquivo_de_pesos.write(linha_pesos_de_documento)
-
-        return {
-
-        }
-
-def similaridade_vetorial(base_arquivo, consulta_arquivo):
-        with open("resposta.txt", "w", encoding='utf-8') as resposta_arquivo:
-            resposta_arquivo.write(f"{documentos_validos}\n")
-            for documento, similaridade in documentos_similaridade_ordenado:
-                if similaridade >= 0.001:
-                    resposta_arquivo.write(f"{documento}: {similaridade}\n")
-
-"""
+        precisao.append(aux_precisao)
+        revocacao.append(aux_revocacao)
+    print(precisao)
+    print(revocacao)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Entrada errada, favor chamar:  > python avaliacao.py referencia.txt")
-        sys.exit(1)
+        sys.exit("Erro: Número incorreto de argumentos")
 
     arquivo_referencia = sys.argv[1]
-    pre_processamento(arquivo_referencia)
-
-    # similaridade_vetorial(arquivo_base, arquivo_consulta)
+    cobertura(arquivo_referencia)
---
+++ trabalho 3/teste.py
@@ -0,0 +1,2 @@
+parma = [1,2,3,4,5,6]
+print(parma)
